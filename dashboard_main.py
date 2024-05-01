3903d92cd4118e6a20a5a0d8b02dc3358203ee84
nam32 <tjanjumratsang@gmail.com>
2024-05-01 18:52:35 +0200

    add comments to main dashboard

--- dashboard_main.py
+++ dashboard_main.py
@@ -1,3 +1,4 @@
+# Import packages
 from dash import Dash, html, dcc, Input, Output, callback 
 import pandas as pd   
 import plotly.graph_objects as go
@@ -13,9 +14,10 @@ os.chdir(directory)
 # https://dash.plotly.com/basic-callbacks
 app = Dash(__name__)
 
-# use your path to the excel file with data
+# Use your path to the excel file with data (this will be changed later to an online version or possibly a real time version)
 df = pd.read_excel(".//who_ambient_air_quality_database_version_2024_(v6.1).xlsx", sheet_name="Update 2024 (V6.1)")
 
+# Airquality are usually measured in aqi when target app audience are general public for ease of data intepretation and we will do that here using lerp and calculate_aqi function
 def lerp(low_aqi, high_aqi, low_conc, high_conc, conc):
     return low_aqi + (conc - low_conc) * (high_aqi - low_aqi) / (high_conc - low_conc)
 
@@ -26,7 +28,9 @@ def calculate_aqi(pollutant_type, concentrations):
             aqi_values.append(None)
             continue
         
-        conc = max(conc, 0)  # Ensure no negative concentrations
+        conc = max(conc, 0)
+        
+        # define pollutant types and their thresholds
         if pollutant_type == 'no2':
             breakpoints = [(0, 53, 0, 50), (54, 100, 51, 100), (101, 360, 101, 150),
                            (361, 649, 151, 200), (650, 1249, 201, 300), (1250, 1649, 301, 400),
@@ -42,7 +46,8 @@ def calculate_aqi(pollutant_type, concentrations):
         else:
             raise ValueError("Unsupported pollutant type")
 
-        aqi = 500  # Default AQI if concentration is beyond the highest range
+        # Default AQI if concentration is beyond the highest range
+        aqi = 500  
         for (low_conc, high_conc, low_aqi, high_aqi) in breakpoints:
             if low_conc <= conc <= high_conc:
                 aqi = lerp(low_aqi, high_aqi, low_conc, high_conc, conc)
@@ -52,14 +57,16 @@ def calculate_aqi(pollutant_type, concentrations):
     
     return aqi_values
 
+# Applying function to pollutants data and add aqi converted data to the columns
 df["pm25_aqi"] = pd.DataFrame(calculate_aqi("pm25", (df["pm25_concentration"]).to_numpy()))
 df["pm10_aqi"] = pd.DataFrame(calculate_aqi("pm10", (df["pm10_concentration"]).to_numpy()))
 df["no2_aqi"] = pd.DataFrame(calculate_aqi("no2", (df["no2_concentration"]).to_numpy()))
 
+# Define data column to fetch on dropdown interactive filter
 pollutants=["pm25_aqi", "pm10_aqi", "no2_aqi"]
 pollutants_options = [{'label': name, 'value': name} for name in pollutants]
 
-# app layout
+# App layout
 app.layout = html.Div([
     html.Div([
 
@@ -76,15 +83,16 @@ app.layout = html.Div([
     dcc.Graph(id='indicator-graphic')
 ])
 
-    # values from dropdown list
+# Values from dropdown list
 @callback(
     Output('indicator-graphic', 'figure'),
     Input('indicator-dropdown', 'value')
     )
 
+# Update graphs interactively
 def update_graph(pollutant):
 
-    # define dictionaries
+    # Define dictionaries for continents
     regions_dict = {
         '1_Afr': 'African',
         '2_Amr': 'Americas',
@@ -95,6 +103,7 @@ def update_graph(pollutant):
         '7_NonMS': 'non-member state'
     }
 
+    # Define legends dictionary
     legends = {
         'pm25_aqi': 'pm 10 aqi',
         'pm10_aqi': 'pm 2.5 aqi',
@@ -105,12 +114,14 @@ def update_graph(pollutant):
     columns=['year']
     indices=['who_region']
 
+    # Data cleaning (dropping rows with no relevant data)
     dff=df.dropna(subset=['iso3', 'year'])
     dff=dff.drop_duplicates()
     dff=dff.drop_duplicates(subset=['iso3', 'city', 'year', 'pm10_concentration', 'pm25_concentration', 'no2_concentration'])
 
     df_pollutant_mean_year = pd.pivot_table(data=dff, index=indices, columns=columns, aggfunc='mean', values=pollutants)
 
+    # First graph plotting
     fig = go.Figure()
     table_for_plot = df_pollutant_mean_year[pollutant].T
     for region in df["who_region"].unique():
