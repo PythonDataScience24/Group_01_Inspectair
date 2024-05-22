984af71ac89fbd3f4b3af2de2197ee9cb9b8a88a
Timsched <schlatti999@gmail.com>
2024-05-22 23:40:33 +0200

    added unittest for calculate aqi

---
+++ tests/calculate_aqi_test.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import unittest
+
+script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'scripts'))
+sys.path.append(script_path)
+
+from datahandling import calculate_aqi
+
+class TestAqi(unittest.TestCase):
+    def test_calculate_aqi(self):
+        self.assertEqual(calculate_aqi("no2",[78,217]),[77,123])
+        self.assertEqual(calculate_aqi("pm25",[35.4]),[100])
+        self.assertEqual(calculate_aqi("pm25",[-22]),[0])
+        self.assertEqual(calculate_aqi("pm10",[177]),[112])
+
+    def test_input_value(self):
+        self.assertRaises(TypeError,calculate_aqi,222,[78,217])
+        self.assertRaises(TypeError,calculate_aqi,"pm25",78)
+
+
+
